eff457e7190b5b0799fe2081061917fb0ab08abe
baratharon <baratharon@caesar.elte.hu>
2015-11-23 14:53:27 +0100

    erlang parser 28th Oct, 10:20

--- erlang/parser.erl
+++ erlang/parser.erl
@@ -76,9 +76,21 @@ preprocess(RevAcc, [{'?', _}, {var, Line, Name} | MoreTokens], State, Mode) ->
 		soft -> preprocess(lists:append(Resolved, RevAcc), RestTokens, State, Mode);
 		_    -> {error, unknown_mode, Mode}
 	end;
+preprocess(RevAcc, [{'#', _}, {atom, Line, RecName}, {'{', _} | MoreTokens], State, Mode) ->
+	case head_is_end_of_expr(RevAcc) of
+		true ->
+			{Expr, NewRevAcc} = fetch_last_expr(RevAcc),
+			{ok, Replacement, RestTokens} = resolve_record_update(RecName, Expr, Line, MoreTokens, State);
+		false ->
+			NewRevAcc = RevAcc,
+			{ok, Replacement, RestTokens} = resolve_record_expr(RecName, Line, MoreTokens, State)
+	end,
+	preprocess(lists:append(Replacement, NewRevAcc), RestTokens, State, Mode);
+%% TODO: ha ')', '}' vagy var van elotte, akkor update (mar preprocesszalt, de ez mindegy is)
+%% TODO: ugyanez csak '=' helyett '.' -> member access
 preprocess(RevAcc, [Token | MoreTokens], State, Mode) ->
 	preprocess([Token | RevAcc], MoreTokens, State, Mode);
-preprocess(RevAcc, [], _, _) ->
+preprocess(RevAcc, [], _State, _Mode) ->
 	lists:reverse(RevAcc).
 
 continue_preprocess_and_parse(RevAcc, Forms, [{macro, MacroName, MacroDef} | Tail], State) ->
@@ -96,6 +108,10 @@ continue_preprocess_and_parse(RevAcc, Forms, [{import, ImportModuleName, ImportF
 	Imports = maps:get(imports, State, []),
 	NewImports = register_imports(Imports, ImportModuleName, ImportFAs),
 	continue_preprocess_and_parse(RevAcc, Forms, Tail, State#{imports => NewImports});
+continue_preprocess_and_parse(RevAcc, Forms, [{record, RecordName, RecordInfo} | Tail], State) ->
+	% record attribute found
+	Records = maps:get(records, State, #{}),
+	continue_preprocess_and_parse(RevAcc, Forms, Tail, State#{records => Records#{RecordName => RecordInfo}});
 continue_preprocess_and_parse(RevAcc, Forms, [], State) ->
 	% no unprocessed attribute left
 	preprocess_and_parse(RevAcc, Forms, State).
@@ -104,6 +120,61 @@ register_imports(ImportList, ImportModuleName, [{FuncName, Arity} | FAs]) ->
 	register_imports([{ImportModuleName, FuncName, Arity}|ImportList], ImportModuleName, FAs);
 register_imports(ImportList, _ImportModuleName, []) ->
 	ImportList.
+
+head_is_end_of_expr([Head | _Tail]) ->
+	case Head of
+		{')', _}    -> true;
+		{'}', _}    -> true;
+		{']', _}    -> true;
+		{var, _, _} -> true;
+		_           -> false
+	end;
+head_is_end_of_expr(_) ->
+	false.
+
+fetch_last_expr(RevExprs) ->
+	{Expr, RevTail} = fetch_last_expr([], RevExprs),
+	{lists:reverse(Expr), RevTail}.
+
+fetch_last_expr(Acc, [Head={Close, _} | Tail]) ->
+	case Close of
+		')' -> fetch_last_expr_impl([Head | Acc], Tail, 0, '(', ')', fun fetch_paren_head/2);
+		'}' -> fetch_last_expr_impl([Head | Acc], Tail, 0, '{', '}', fun fetch_brace_head/2);
+		']' -> fetch_last_expr_impl([Head | Acc], Tail, 0, '[', ']', undefined)
+	end;
+fetch_last_expr(Acc, [Head={var, _, _} | Tail]) ->
+	{[Head | Acc], Tail}.
+
+fetch_paren_head(Acc, [H1={atom, _, _}, H2={':', _}, H3={atom, _, _} | Tail]) ->
+	% remote function call
+	{[H3, H2, H1 | Acc], Tail};
+fetch_paren_head(Acc, [H1={Type, _, _} | Tail]) when atom==Type; var==Type ->
+	% local function call, or fun variable
+	{[H1 | Acc], Tail};
+fetch_paren_head(Acc, Tail) ->
+	% simple parenthesis
+	{Acc, Tail}.
+
+fetch_brace_head(Acc, [H1={'#', _} | Tail]) ->
+	case head_is_end_of_expr(Tail) of
+		true -> fetch_last_expr([H1 | Acc], Tail);
+		_    -> {[H1 | Acc], Tail}
+	end;
+fetch_brace_head(Acc, Tail) ->
+	% tuple
+	{Acc, Tail}.
+
+fetch_last_expr_impl(Acc, [Head={Open, _} | Tail], 0, Open, _Close, Fun) ->
+	case is_function(Fun) of
+		true -> Fun([Head | Acc], Tail);
+		_    -> {[Head | Acc], Tail}
+	end;
+fetch_last_expr_impl(Acc, [Head={Open, _} | Tail], Depth, Open, Close, Fun) ->
+	fetch_last_expr_impl([Head | Acc], Tail, Depth+1, Open, Close, Fun);
+fetch_last_expr_impl(Acc, [Head={Close, _} | Tail], Depth, Open, Close, Fun) ->
+	fetch_last_expr_impl([Head | Acc], Tail, Depth-1, Open, Close, Fun);
+fetch_last_expr_impl(Acc, [Head | Tail], Depth, Open, Close, Fun) ->
+	fetch_last_expr_impl([Head | Acc], Tail, Depth, Open, Close, Fun).
 
 resolve('LINE', Line, RestTokens, _State, _Mode) ->
 	{ok, [{integer, Line, Line}], RestTokens};
@@ -205,6 +276,9 @@ read_attribute([{atom, Line, define}, {'(', _}, {var, _, DefName}, {',', _} | De
 read_attribute([{atom, Line, define}, {'(', _}, {var, _, DefName}, {'(', _} | DefTail], Forms) ->
 	% defining parametric macros
 	parse_parametric_macro(DefName, Line, DefTail, Forms);
+read_attribute([{atom, _, record}, {'(', _}, {atom, _, RecName}, {',', _}, {'{', _} | Tail], Forms) ->
+	% defining record (will be translated)
+	parse_record(RecName, Tail, Forms);
 read_attribute([{atom, _, export} | _], Forms) ->
 	% drop the 'export' now, we don't need it yet
 	{ok, [], Forms};
@@ -264,3 +338,98 @@ parse_imports(ModuleName, [{',', _} | Rest], FAs, Forms) ->
 	parse_imports(ModuleName, Rest, FAs, Forms);
 parse_imports(ModuleName, [{']', _}, {')', _}, {dot, _}], FAs, Forms) ->
 	{ok, [{import, ModuleName, FAs}], Forms}.
+
+parse_record(RecName, Tail, Forms) ->
+	parse_record(RecName, #{}, [], 2, Tail, Forms).
+
+parse_record(RecName, FieldMap, FieldList, NextFieldId, [{atom, _, Field}, {',', _} | Tail], Forms) ->
+	parse_record(RecName, FieldMap#{Field => {NextFieldId, undefined}}, [Field | FieldList], NextFieldId+1, Tail, Forms);
+parse_record(RecName, FieldMap, FieldList, NextFieldId, [{atom, _, Field}, {'}', _} | Tail], Forms) ->
+	build_record_info(RecName, FieldMap#{Field => {NextFieldId, undefined}}, [Field | FieldList], NextFieldId, Tail, Forms).
+
+build_record_info(RecName, FieldMap, FieldList, Size, [{')', _}, {dot, _}], Forms) ->
+	{ok, [{record, RecName, {RecName, Size, FieldMap, FieldList}}], Forms}.
+
+resolve_record_update(RecName, Expr, Line, MoreTokens, State) ->
+	{ok, Records} = maps:find(records, State),
+	{ok, {RecName, _Size, FieldMap, _FieldList}} = maps:find(RecName, Records),
+	resolve_record_update_impl(Expr, Line, [], FieldMap, MoreTokens).
+
+resolve_record_update_impl(Expr, _Line, _ModifiedFields, _FieldMap, [{'}', _} | RestTokens]) ->
+	{ok, Expr, RestTokens};
+resolve_record_update_impl(Expr, Line, ModifiedFields, FieldMap, [{atom, _, Field}, {'=', _} | MoreTokens]) ->
+	{ValueExpr, MoreTokens2} = parse_record_field_assign(MoreTokens),
+	{ok, {FieldId, _Initial}} = maps:find(Field, FieldMap),
+	SetElementExpr = make_setelement_expr(FieldId, Expr, ValueExpr, Line),
+	resolve_record_update_impl(SetElementExpr, Line, [Field | ModifiedFields], FieldMap, MoreTokens2).
+
+resolve_record_expr(RecName, Line, MoreTokens, State) ->
+	Records = maps:get(records, State, #{}),
+	{ok, RecordInfo={RecName, _Size, _FieldMap, _FieldList}} = maps:find(RecName, Records),
+	resolve_record_expr(RecName, RecordInfo, Line, #{}, MoreTokens).
+
+resolve_record_expr(RecName, RecordInfo, Line, FieldAssigns, [{'}', _} | RestTokens]) ->
+	{RecName, _Size, FieldMap, FieldList} = RecordInfo,
+	FinalAssigns = maps:merge(FieldMap, FieldAssigns),
+	{ok, make_tuple_from_record(RecName, FieldList, FinalAssigns, Line), RestTokens};
+resolve_record_expr(RecName, RecordInfo, Line, FieldAssigns, [{atom, _, Field}, {'=', _} | MoreTokens]) ->
+	{Expr, MoreTokens2} = parse_record_field_assign(MoreTokens),
+	resolve_record_expr(RecName, RecordInfo, Line, FieldAssigns#{Field => Expr}, MoreTokens2);
+resolve_record_expr(RecName, RecordInfo, Line, FieldAssigns, [{var, _, '_'}, {'=', _} | MoreTokens]) ->
+	{Expr, MoreTokens2} = parse_record_field_assign(MoreTokens),
+	{RecName, _Size, _FieldMap, FieldList} = RecordInfo,
+	NewFieldAssigns = fill_remaining_field_assigns(FieldAssigns, FieldList, Expr),
+	resolve_record_expr(RecName, RecordInfo, Line, NewFieldAssigns, MoreTokens2).
+
+parse_record_field_assign(Tokens) ->
+	parse_record_field_assign([], Tokens).
+
+parse_record_field_assign(Acc, [{',', _} | RestTokens]) ->
+	{Acc, RestTokens};
+parse_record_field_assign(Acc, [{'}', _} | _] = RestTokens) ->
+	{Acc, RestTokens};
+parse_record_field_assign(Acc, [Token | Tokens]) ->
+	parse_record_field_assign([Token | Acc], Tokens).
+
+fill_remaining_field_assigns(Assigns, [], _Expr) ->
+	Assigns;
+fill_remaining_field_assigns(Assigns, [Field | Fields], Expr) ->
+	case maps:find(Field, Assigns) of
+		error -> fill_remaining_field_assigns(Assigns#{Field => Expr}, Fields, Expr);
+		_     -> fill_remaining_field_assigns(Assigns, Fields, Expr)
+	end.
+
+make_tuple_from_record(RecordName, RevFieldList, Assigns, Line) ->
+	make_tuple_from_record_impl([{atom, Line, RecordName}, {'{', Line}], lists:reverse(RevFieldList), Assigns, Line).
+
+make_tuple_from_record_impl(Replacement, [Field | Fields], Assigns, Line) ->
+	{ok, Value} = maps:find(Field, Assigns),
+	case Value of
+		{_FieldId, undefined} -> ActualValue = [{atom, Line, undefined}];
+		_                     -> ActualValue = Value
+	end,
+	make_tuple_from_record_impl(ActualValue ++ [{',', Line}] ++ Replacement, Fields, Assigns, Line);
+make_tuple_from_record_impl(Replacement, [], _Assigns, Line) ->
+	[{'}', Line} | Replacement].
+
+make_setelement_expr(FieldId, Expr, ValueExpr, Line) ->
+	[
+		{')', Line},
+		{')', Line}
+	]
+	++ ValueExpr ++
+	[
+		{'(', Line},
+		{',', Line},
+		{')', Line}
+	]
+	++ Expr ++
+	[
+		{'(', Line},
+		{',', Line},
+		{integer, Line, FieldId},
+		{'(', Line},
+		{atom, Line, setelement},
+		{':', Line},
+		{atom, Line, erlang}
+	].
