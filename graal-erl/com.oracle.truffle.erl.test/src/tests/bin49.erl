e0dc1cd343a45bea3a38a900c592e7a93b2bc0f6
baratharon <baratharon@caesar.elte.hu>
2015-12-04 11:10:44 +0100

    new test case for binary_to_list/3

---
+++ graal-erl/com.oracle.truffle.erl.test/src/tests/bin49.erl
@@ -0,0 +1,14 @@
+-module(bin49).
+-export([main/0]).
+
+main() ->
+	Bin = <<1,2,3,4, 5:5>>,
+	{
+		try binary_to_list(Bin, 0, 0) catch error:badarg -> {ok} end,
+		try binary_to_list(Bin, 4, 5) catch error:badarg -> {ok} end,
+		binary_to_list(Bin, 1, 1),
+		binary_to_list(Bin, 1, 2),
+		binary_to_list(Bin, 1, 3),
+		binary_to_list(Bin, 1, 4),
+		binary_to_list(Bin, 2, 3)
+	}.
