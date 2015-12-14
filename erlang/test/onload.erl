1e9cca3699818e5f0fa78ffbfdbd4bb56a56a34f
baratharon <baratharon@caesar.elte.hu>
2015-12-14 11:59:49 +0100

    new entry point in onload.erl

--- erlang/test/onload.erl
+++ erlang/test/onload.erl
@@ -1,11 +1,14 @@
 -module(onload).
--export([main/0]).
+-export([main/0, main/1]).
 -on_load(onload/0).
 -define(ON_LOAD_KEY, my_key).
 
 onload() ->
 	put(?ON_LOAD_KEY, yup),
 	ok.
+
+main(_) ->
+	main().
 
 main() ->
 	io:format("~p~n", [get(?ON_LOAD_KEY)]).
